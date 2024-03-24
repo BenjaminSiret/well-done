534b60aadb36eac776f31ae50997c9a39e5084d4
Benjamin Siret <benjaminsiret@outlook.fr>
2024-03-24 10:56:41 +0100

    create shader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import Footer from "@/components/Footer";
 import TaskForm from "./components/TaskForm";
 import TaskCard from "./components/TaskCard";
+import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
+import * as reactSpring from "@react-spring/three";
+import * as drei from "@react-three/drei";
+import * as fiber from "@react-three/fiber";
+import { animate } from "framer-motion";
 
 export default function App() {
   const [tasks, setTasks] = useState(() => {
@@ -13,6 +18,8 @@ export default function App() {
     }
   });
 
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
@@ -21,25 +28,54 @@ export default function App() {
     setTasks((prevTasks) => {
       return [...prevTasks, data];
     });
+
+    setIsFormSubmitted(true);
+
+    setTimeout(() => {
+      setIsFormSubmitted(false);
+    }, 1500);
   };
 
   return (
     <>
       <div className="flex flex-col min-h-screen">
+        <ShaderGradientCanvas
+          importedfiber={{ ...fiber, ...drei, ...reactSpring }}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            pointerEvents: "none",
+            zIndex: -1,
+          }}
+        >
+          <ShaderGradient
+            control="query"
+            urlString="https://www.shadergradient.co/customize?animate=off&axesHelper=off&brightness=1.2&cAzimuthAngle=170&cDistance=4.4&cPolarAngle=70&cameraZoom=1&color1=%2394ffd1&color2=%236bf5ff&color3=%23ffffff&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0.9&positionZ=-0.3&range=enabled&rangeEnd=39.6&rangeStart=0&reflection=0.1&rotationX=45&rotationY=0&rotationZ=0&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.2&uFrequency=0&uSpeed=0.2&uStrength=3.4&uTime=0&wireframe=false"
+          />
+        </ShaderGradientCanvas>
         <main className="flex flex-col flex-grow items-center justify-center space-y-5 md:space-y-8 lg:space-y-11">
-          <h1 className="text-7xl font-extrabold text-center">Well done!</h1>
+          <h1
+            className={`text-transparent text-6xl font-extrabold text-center gradient-text ${
+              isFormSubmitted ? "animate-gradient" : ""
+            }`}
+          >
+            Well done!
+          </h1>
           <h2 className="text-center text-3xl">Start tracking your achievements today</h2>
           <TaskForm newTaskHandler={newTaskHandler} />
-          {tasks &&
+          {/* {tasks &&
             tasks.map((task, index) => {
               return (
                 <div key={index} className="flex justify-center gap-5">
                   <TaskCard task={task} />
                 </div>
               );
-            })}
+            })} */}
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -66,10 +66,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        gradient: {
+          "0%": { backgroundPosition: '0% 50%' },
+          "100%": { backgroundPosition: '100% 50%' },
+        },
+        scale: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+          "100%": { transform: "scale(1)" }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        gradient: 'gradient 1.5s linear',
       },
     },
   },
