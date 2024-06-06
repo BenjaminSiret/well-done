5c72a541ea16d435b5b0e41f41dc827798fdfcbc
Benjamin Siret <benjaminsiret@outlook.fr>
2024-06-06 17:01:00 +0200

    create ThemeToggle

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -4,14 +4,18 @@ import { useMediaQuery } from "@/hooks/use-media-query";
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
 
 const MainMenu = () => {
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return isDesktop ? (
-    <div>Desktop</div>
+    <div className="flex">
+      <div>Desktop</div>
+      <ThemeToggle className="ml-auto mt-2 mr-2" />
+    </div>
   ) : (
-    <div className="p-1 sticky top-0">
+    <div className="flex p-1 sticky top-0">
       <Sheet>
         <SheetTrigger>
           <HamburgerMenuIcon className="w-5 h-5" />
@@ -25,6 +29,7 @@ const MainMenu = () => {
           </Link>
         </SheetContent>
       </Sheet>
+      <ThemeToggle className="ml-auto" />
     </div>
   );
 };
