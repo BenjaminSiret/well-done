57288141bf72124ac7a206537336bc2cc396bea6
Benjamin Siret <benjaminsiret@outlook.fr>
2024-06-14 18:25:44 +0200

    create formatDate function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,15 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format } from "date-fns"
 
-export function cn(...inputs) {
+export function cn (...inputs) {
   return twMerge(clsx(inputs))
 }
 
-export function pluralize(count, singular, plural) {
+export function pluralize (count, singular, plural) {
   return count === 1 ? singular : plural
+}
+
+export function formatDate (timestamp) {
+  return format(new Date(timestamp), "PP")
 }
